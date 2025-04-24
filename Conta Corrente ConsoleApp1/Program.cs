8c40c8732074bbe2c7ae1006f67aa4243873792b
caues <caues@SCHMITZNOT>
2025-04-23 21:50:09 -0300

    Add project files.

---
+++ Conta Corrente ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Conta_Corrente_ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CriarConta conta1 = new CriarConta();
+            CriarConta conta2 = new CriarConta();
+
+            Operacoes.Depositar(conta1, -300);
+            Operacoes.Depositar(conta1, 100);
+            Operacoes.ConsultarSaldo(conta1);
+            Operacoes.Transferir(conta1, conta2, 50);
+            Operacoes.Sacar(conta1, 200);
+            Operacoes.EmitirExtrato(conta1);
+
+            Operacoes.Depositar(conta2, 500);
+            Operacoes.Sacar(conta2, 1000);
+            Operacoes.ConsultarSaldo(conta2);
+            Operacoes.EmitirExtrato(conta2);
+            Console.ReadLine();
+        }
+    }
+}
