8104e64b3d3ac77d7eb737bed41242126576ee59
caues <caues@SCHMITZNOT>
2025-04-24 20:21:59 -0300

    implementada versao completa e atualizada

--- Conta Corrente ConsoleApp1/ContaCorrente.cs
+++ Conta Corrente ConsoleApp1/ContaCorrente.cs
@@ -1,25 +1,41 @@
-﻿
-namespace Conta_Corrente_ConsoleApp1
+﻿namespace ContaCorrente.ConsoleApp
 {
+
     public class ContaCorrente
     {
-        //atributos
+        // atributos
         public int numero;
         public decimal saldo;
         public decimal limite;
+        public Movimentacao[] movimentacoes;
+        int contadorMovimentacoes = 0;
 
-        //metodos
-
+        // metodos
         public void Sacar(decimal quantia)
         {
             if (quantia <= saldo + limite)
             {
                 saldo -= quantia;
+
+                Movimentacao novaMovimentacao = new Movimentacao();
+                novaMovimentacao.valor = quantia;
+                novaMovimentacao.tipo = "Débito";
+
+                movimentacoes[contadorMovimentacoes] = novaMovimentacao;
+                contadorMovimentacoes++;
             }
         }
+
         public void Depositar(decimal quantia)
         {
             saldo += quantia;
+
+            Movimentacao novaMovimentacao = new Movimentacao();
+            novaMovimentacao.valor = quantia;
+            novaMovimentacao.tipo = "Crédito";
+
+            movimentacoes[contadorMovimentacoes] = novaMovimentacao;
+            contadorMovimentacoes++;
         }
 
         public void TransferirPara(ContaCorrente destinatario, decimal quantia)
@@ -28,8 +44,24 @@ namespace Conta_Corrente_ConsoleApp1
 
             this.Sacar(quantia);
         }
-    }
-    }
-    
-    
 
+        public void ExibirExtrato()
+        {
+            Console.WriteLine("Extrato da Conta #" + this.numero);
+
+            Console.WriteLine("Saldo: " + this.saldo);
+
+            Console.WriteLine();
+
+            for (int i = 0; i < movimentacoes.Length; i++)
+            {
+                Movimentacao movimentacaoAtual = movimentacoes[i];
+
+                if (movimentacaoAtual != null)
+                {
+                    Console.WriteLine(movimentacaoAtual.LerMovimentacao());
+                }
+            }
+        }
+    }
+}
---
+++ Conta Corrente ConsoleApp1/Movimetacao.cs
@@ -0,0 +1,13 @@
+﻿namespace ContaCorrente.ConsoleApp
+{
+    public class Movimentacao
+    {
+        public decimal valor;
+        public string tipo;
+
+        public string LerMovimentacao()
+        {
+            return $"{tipo} - {valor.ToString("C2")}";
+        }
+    }
+}
