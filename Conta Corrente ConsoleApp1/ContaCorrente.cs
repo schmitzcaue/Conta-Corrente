e79551c37eb4b2a9343aa0ec69047d5a78fee306
caues <caues@SCHMITZNOT>
2025-04-24 19:50:22 -0300

    implementado operacoes

---
+++ Conta Corrente ConsoleApp1/ContaCorrente.cs
@@ -0,0 +1,35 @@
+﻿
+namespace Conta_Corrente_ConsoleApp1
+{
+    public class ContaCorrente
+    {
+        //atributos
+        public int numero;
+        public decimal saldo;
+        public decimal limite;
+
+        //metodos
+
+        public void Sacar(decimal quantia)
+        {
+            if (quantia <= saldo + limite)
+            {
+                saldo -= quantia;
+            }
+        }
+        public void Depositar(decimal quantia)
+        {
+            saldo += quantia;
+        }
+
+        public void TransferirPara(ContaCorrente destinatario, decimal quantia)
+        {
+            destinatario.Depositar(quantia);
+
+            this.Sacar(quantia);
+        }
+    }
+    }
+    
+    
+
--- Conta Corrente ConsoleApp1/Program.cs
+++ Conta Corrente ConsoleApp1/Program.cs
@@ -1,25 +1,51 @@
-﻿namespace Conta_Corrente_ConsoleApp1
+﻿
+namespace Conta_Corrente_ConsoleApp1
 {
+
     internal class Program
     {
-        static void Main(string[] args)
-        {
-            CriarConta conta1 = new CriarConta();
-            CriarConta conta2 = new CriarConta();
+        
 
-            Operacoes.Depositar(conta1, -300);
-            Operacoes.Depositar(conta1, 100);
-            Operacoes.ConsultarSaldo(conta1);
-            Operacoes.Transferir(conta1, conta2, 50);
-            Operacoes.Sacar(conta1, 200);
-            Operacoes.EmitirExtrato(conta1);
+            static void Main(string[] args)
 
-            Operacoes.Depositar(conta2, 500);
-            Operacoes.Sacar(conta2, 1000);
-            Operacoes.ConsultarSaldo(conta2);
-            Operacoes.EmitirExtrato(conta2);
+            {
+                ContaCorrente conta1 = new ContaCorrente();
+                conta1.numero = 12;
+                conta1.saldo = 1000;
+                conta1.limite = 0;
+
+
+                conta1.Sacar(200);
+
+                conta1.Depositar(300);
+
+                conta1.Depositar(500);
+
+                conta1.Sacar(200);
+
+                ContaCorrente conta2 = new ContaCorrente();
+
+                conta2.saldo = 300;
+
+                conta2.numero = 13;
+
+                conta2.limite = 0;
+
+
+                conta1.TransferirPara(conta2, 400);
+
+                conta1.ExibirExtrato();
+
+                Console.WriteLine();
+
+
+                conta2.ExibirExtrato();
+            Console.WriteLine(); (conta1.saldo);
             Console.ReadLine();
+            }
+
         }
     }
     
-}
+    
+
